a41587b81d623bd394e14b7ad1bf75d747773cf4
Arjun Dhakal <arjundhakal8330@gmail.com>
2025-12-22 16:35:30 +0545

    models commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Blog
 
+
+
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'status', 'is_featured',)
+    search_fields = ('title', 'author__username',)
+    list_editable=('is_featured',)
+
+
+
+    
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+STATUS_CHOICES=(
+    (0,'Draft'),
+    (1,'Published')
+)
+
+class Blog(models.Model):
+    title=models.CharField(max_length=200)
+    slug=models.SlugField(max_length=200,unique=True)
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='blogs_images/')
+    short_description=models.TextField(max_length=300)
+    blog_content=models.TextField()
+    status=models.IntegerField(choices=STATUS_CHOICES,default=0)
+    is_featured=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+
+    
